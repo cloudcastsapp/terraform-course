ae452c7a08eb4e5eb84fc34e1d35c914c658c841
Chris Fidao <fideloper@gmail.com>
2021-01-28 09:38:53 -0600

    data and resource: first ec2 instance

--- cloudcasts.tf
+++ cloudcasts.tf
@@ -11,3 +11,39 @@ provider "aws" {
   profile = "cloudcasts"
   region = "us-east-2"
 }
+
+data "aws_ami" "app" {
+  most_recent = true
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "tag:Component"
+    values = ["app"]
+  }
+
+  filter {
+    name   = "tag:Project"
+    values = ["cloudcast"]
+  }
+
+  filter {
+    name   = "tag:Environment"
+    values = ["staging"]
+  }
+
+  owners = ["self"]
+}
+
+resource "aws_instance" "cloudcasts_web" {
+  ami           = data.aws_ami.app.id
+  instance_type = "t3.small"
+
+  root_block_device {
+    volume_size = 8 # GB
+    volume_type = "gp3"
+  }
+}
