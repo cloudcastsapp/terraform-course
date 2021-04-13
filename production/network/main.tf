06efcd1e2f08bc7e7c56d38583f9fe9f4a5e0684
Chris Fidao <fideloper@gmail.com>
2021-04-13 14:42:38 -0500

    use locals in network state area

--- production/network/main.tf
+++ production/network/main.tf
@@ -29,13 +29,17 @@ variable default_region {
   default = "us-east-2"
 }
 
+locals {
+    cidr_subnets = cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4, 4, 4, 4)
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 
   infra_env = var.infra_env
   vpc_cidr = "10.0.0.0/17"
   azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  public_subnets = slice(cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4, 4, 4, 4), 0, 3)
-  private_subnets = slice(cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4, 4, 4, 4), 3, 6)
-  database_subnets = slice(cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4, 4, 4, 4), 6, 9)
+  public_subnets = slice(local.cidr_subnets, 0, 3)
+  private_subnets = slice(local.cidr_subnets, 3, 6)
+  database_subnets = slice(local.cidr_subnets, 6, 9)
 }
